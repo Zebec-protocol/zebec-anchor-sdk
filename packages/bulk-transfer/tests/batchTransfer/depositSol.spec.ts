a1b74dc27e215a4a0c7c94bfd15df680bbe6ade4
Ashish Sapkota <aacspkt3@gmail.com>
2023-03-30 15:38:40 +0545

    test script for batch token transfer

--- packages/bulk-transfer/tests/batchTransfer/depositSol.spec.ts
+++ packages/bulk-transfer/tests/batchTransfer/depositSol.spec.ts
@@ -1,19 +1,28 @@
-import { describe, it } from "mocha";
+import {
+	describe,
+	it,
+} from "mocha";
 
-import { BatchTransferInstruction, BatchTransferService, ProgramFactory } from "../../src";
+import {
+	BatchTransferInstruction,
+	BatchTransferService,
+	ProgramFactory,
+} from "../../src";
 import { provider } from "../shared";
 
-const batchTransferIxns = new BatchTransferInstruction(ProgramFactory.getBatchTranferProgram({}));
-const service = new BatchTransferService(provider, batchTransferIxns);
-
 describe("depositSol()", () => {
+	const batchTransferIxns = new BatchTransferInstruction(ProgramFactory.getBatchTranferProgram({}));
+	const service = new BatchTransferService(provider, batchTransferIxns);
+	
 	it("deposit sol to vault", async () => {
 		const feePayer = provider.publicKey;
 		const authority = feePayer;
-		const amount = 2;
+		const amount = 1.2353523;
 		const depositSolPayload = await service.depositSol(authority, amount);
 		const signature = await depositSolPayload.execute();
 
 		console.log("signature", signature);
+		
+
 	});
 });

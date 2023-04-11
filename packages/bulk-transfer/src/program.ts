1b2c83c9c288649d7e23802b274ce460449c5496
Ashish Sapkota <aacspkt3@gmail.com>
2023-04-11 15:43:58 +0545

    update Program factory

--- packages/bulk-transfer/src/program.ts
+++ packages/bulk-transfer/src/program.ts
@@ -3,11 +3,8 @@ import { AnchorProvider, Program } from "@project-serum/anchor";
 import { BatchTransfer, BatchTransferIdl } from "./artifacts";
 import { BATCH_TRANFER_PROGRAM_ID } from "./constants";
 
-export class ProgramFactory {
-	static getBatchTranferProgram({
-		programId = BATCH_TRANFER_PROGRAM_ID,
-		provider = AnchorProvider.env(),
-	}): Program<BatchTransfer> {
+export class BatchTranferProgramFactory {
+	static getProgram({ programId = BATCH_TRANFER_PROGRAM_ID, provider = AnchorProvider.env() }): Program<BatchTransfer> {
 		return new Program(BatchTransferIdl as BatchTransfer, programId, provider);
 	}
 }

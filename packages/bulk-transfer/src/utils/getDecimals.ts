8bf11dcc9ccab1e5e7399af9c1324b891af33d69
Ashish Sapkota <aacspkt3@gmail.com>
2023-04-17 18:11:53 +0545

    add owner validation for mint info

--- packages/bulk-transfer/src/utils/getDecimals.ts
+++ packages/bulk-transfer/src/utils/getDecimals.ts
@@ -1,13 +1,14 @@
 import assert from "assert";
 
 import { web3 } from "@project-serum/anchor";
-import { MintLayout } from "@solana/spl-token";
+import { MintLayout, TOKEN_PROGRAM_ID } from "@solana/spl-token";
 
 export async function getDecimals(connection: web3.Connection, mint: web3.PublicKey): Promise<number> {
-    const mintInfo = await connection.getAccountInfo(mint);
-    assert(mintInfo, "Token not found");
-    assert(mintInfo.data.length == MintLayout.span, "Account size of mint is invalid.")
-    
-    const rawMint = MintLayout.decode(Uint8Array.from(mintInfo.data));
-    return rawMint.decimals;
-};
+	const mintInfo = await connection.getAccountInfo(mint);
+	assert(mintInfo, "Token not found");
+	assert(mintInfo.owner.equals(TOKEN_PROGRAM_ID), "Mint is not owned by Token Program.");
+	assert(mintInfo.data.length === MintLayout.span, "Account size of mint is invalid.");
+
+	const rawMint = MintLayout.decode(Uint8Array.from(mintInfo.data));
+	return rawMint.decimals;
+}

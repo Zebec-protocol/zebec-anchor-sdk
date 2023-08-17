e0b444365ba7fb354ba30086407309e57bcbe499
shrestha-roshan <shrestharoshan768@gmail.com>
2023-08-17 10:16:58 +0545

    test for cron batch transfer sol

---
+++ packages/bulk-transfer/tests/cronBatchTransfer/cronBatchSolTransfer.spec.ts
@@ -0,0 +1,58 @@
+import { describe, it } from "mocha";
+
+import * as anchor from "@project-serum/anchor";
+
+import receivers from "./receivers.json";
+import {
+	BatchSolTransferData,
+	BatchTranferProgramFactory,
+	BatchTransferInstruction,
+	BatchTransferService,
+} from "../../src";
+import { provider } from "../shared";
+
+describe("BatchSolTransfer", () => {
+	const batchTransferIxns = new BatchTransferInstruction(BatchTranferProgramFactory.getProgram({}));
+	const batchTransactionService = new BatchTransferService(provider, batchTransferIxns);
+
+	it("should transfer SOL to multiple accounts using thread", async () => {
+		let batchSolTransferData: BatchSolTransferData[][] = [];
+
+		let receiversAddresses = receivers;
+		for (let i = 0; i < receiversAddresses.length; i++) {
+			let dataPerBatch: BatchSolTransferData[] = [];
+			for (let j = 0; j < receiversAddresses[i].length; j++) {
+				let receiverPubkey = new anchor.web3.PublicKey(receiversAddresses[i][j]);
+				let amount = 0.001;
+				dataPerBatch.push({
+					account: receiverPubkey.toString(),
+					amount: amount,
+				});
+			}
+			batchSolTransferData.push(dataPerBatch);
+		}
+
+		const threadId = "zbc_test_" + new Date().getTime();
+		const threadAuthority = provider.wallet.publicKey.toString();
+		const trigger = {
+			cron: {
+				schedule: "*/30 * * * * * *",
+				skippable: true,
+			},
+		};
+
+		const batchTransferIxn = await batchTransactionService.createTransferSolInBatchThread({
+			authority: threadAuthority,
+			threadId: threadId,
+			triggerInput: trigger,
+			batchData: batchSolTransferData,
+			amountForThread: 0.001,
+		});
+		try {
+			const result = await batchTransferIxn.execute();
+			console.log("result", result);
+		} catch (e) {
+			console.log(e);
+		}
+	});
+});
